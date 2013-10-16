3b73c90f273b549944a1974a430fe47a1ee50f0f
David Tovar Goris <sgoris@gmail.com>
2013-10-15 23:08:22 -0500

    Se suben los archivos faltantes cambiados

---
+++ Models/Characters.cpp
@@ -0,0 +1,23 @@
+//
+//  Glulucat
+//
+//  Created by David Tovar Goris
+//         and Guillermo de los Santos
+//          on 10/15/13.
+//  Copyright (c) 2013 Glulucat. All rights reserved.
+//
+
+#include <iostream>
+#include "Characters.h"
+
+Characters::Characters (void){
+    x = 0;
+    y = 0;
+    x_speed = 0;
+    y_speed = 0;
+    jumping  = false;
+    powerup = false;
+    flicking = false;
+    
+}
+
