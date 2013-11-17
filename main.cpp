a3b656bffd5da5a8bc8bee4662ebc44c682a5b06
David Tovar Goris <sgoris@gmail.com>
2013-11-17 17:05:58 -0600

    Se agrega la pausa

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ Glulucat glulucat;
 Level level;
 std::vector<Duck> ducks;
 
+bool pause;
 
 /*
 * Limpia el fondo
@@ -32,27 +33,29 @@ std::vector<Duck> ducks;
 void init(void) {
     glClearColor (0.0, 0.0, 0.0, 0.0);
     glShadeModel(GL_FLAT);
-
+    
     glulucat = Glulucat();
-
+    
     for(int i = 0; i < 5; i++) {
         Duck duck = Duck();
         ducks.push_back(duck);
     }
-
+    
+    pause = false;
 }
 
-void timer(int una_vars){
+void timer(int una_vars) {
 
-    glulucat.moveY(level.blocks);
-    int i = 0;
-    for(std::vector<Duck>::iterator it = ducks.begin(); it != ducks.end(); ++it, i++ ) {
-        it->moveY(level.blocks);
-        it->moveX(i, level.blocks);
+    if(!pause) {
+        glulucat.moveY(level.blocks);
+        int i = 0;
+        for(std::vector<Duck>::iterator it = ducks.begin(); it != ducks.end(); ++it, i++ ) {
+            it->moveY(level.blocks);
+            it->moveX(i, level.blocks);
+        }
     }
-
     glutPostRedisplay();
-
+    
     glutTimerFunc(25,timer,0);
 }
 
@@ -72,7 +75,6 @@ void display(void) {
 
 }
 
-
 void reshape (int w, int h) {
     glViewport (0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode (GL_PROJECTION);
@@ -81,38 +83,88 @@ void reshape (int w, int h) {
     glMatrixMode(GL_MODELVIEW);
 }
 
-void keyboard (unsigned char key, int x, int y) {
-    switch (key) {
-        case 'A': case 'a':
-            glulucat.moveX(-10, level.blocks);
-
+void processMenu(int option){
+    switch (option) {
+        case 1:
+            
             break;
-
-        case 'D': case 'd':
-            glulucat.moveX(10, level.blocks);
-
+        case 99:
+            pause = !pause;
             break;
-
-        case 'W': case 'w':
-            glulucat.startJumping();
-
-            break;
-
-        case 'S': case 's':
-            glulucat.isOnAir(false);
-
-            break;
-
-        case 27:
-            exit(0);
-            break;
-
         default:
             break;
+    }
+}
+
+void createMenu (void){
+    int mainMenu, levels, others;
+    
+    levels = glutCreateMenu(processMenu);
+    glutAddMenuEntry("Level 1", 1);
+    glutAddMenuEntry("Level 2", 2);
+    
+    others = glutCreateMenu(processMenu);
+    glutAddMenuEntry("Opcion randy", 1);
+    glutAddMenuEntry("Locura!", 2);
+    
+    mainMenu = glutCreateMenu(processMenu);
+    glutAddSubMenu("Niveles", levels);
+    glutAddMenuEntry("Pausa", 99);
+    glutAddSubMenu("Otros", others);
+    
+    // Let the menu respond on the right mouse button
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+    
+}
+
+void keyboard (unsigned char key, int x, int y) {
+    if(!pause) {
+        switch (key) {
+            case 'A': case 'a':
+                glulucat.moveX(-10, level.blocks);
+
+                break;
+
+            case 'D': case 'd':
+                glulucat.moveX(10, level.blocks);
+
+                break;
+
+            case 'W': case 'w':
+                glulucat.startJumping();
+
+                break;
+
+            case 'S': case 's':
+                glulucat.isOnAir(false);
+
+                break;
+
+            case 27:
+                exit(0);
+                break;
+
+            default:
+                break;
+        }
     }
     glutPostRedisplay();
 }
 
+void mouse(int button, int state, int x, int y) {
+    
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            
+            break;
+            
+        case GLUT_RIGHT_BUTTON:
+            if(state == GLUT_UP){
+
+            }
+            break;
+    }
+}
 
 int main (int argc, char** argv) {
 
@@ -122,11 +174,13 @@ int main (int argc, char** argv) {
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize (800, 600);
     glutInitWindowPosition (100, 100);
-    glutCreateWindow (argv[0]);
+    glutCreateWindow ("Glulucat: The almighty cat");
     init ();
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
+    createMenu();
     glutTimerFunc(25, timer, 1);
     glutMainLoop();
 
